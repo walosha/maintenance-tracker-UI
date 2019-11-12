bf916aaf0ed7c77a1ac743d545b6d78f4bb9dcbc
walosha <walosha@yahoo.com>
2019-11-12 02:26:25 +0100

    change  base url to production

--- src/api/maintenancetrackerapp.js
+++ src/api/maintenancetrackerapp.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const maintenancetrackerapp = axios.create({
-  baseURL: "http://127.0.0.1:3000"
+  baseURL: "https://maintenancetrackerapp.herokuapp.com/api/v1"
 });
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,7 +3,7 @@ import { SIGN_IN, SIGN_UP, SIGN_OUT, GET_ME } from "../types/index";
 import axios from "axios";
 
 export const signup = value => async dispatch => {
-  const res = await maintenancetrackerapp.post("/api/v1/users/signup", value);
+  const res = await maintenancetrackerapp.post("/users/signup", value);
   window.localStorage.setItem("jwt", res.data.token);
   console.log(res.data.token);
   dispatch({
@@ -12,7 +12,7 @@ export const signup = value => async dispatch => {
   });
 };
 export const signin = value => async dispatch => {
-  const res = await maintenancetrackerapp.post("/api/v1/users/signin", value);
+  const res = await maintenancetrackerapp.post("/users/signin", value);
   window.localStorage.setItem("jwt", res.data.token);
   console.log(res);
   dispatch({
@@ -22,7 +22,7 @@ export const signin = value => async dispatch => {
 };
 
 export const signout = () => async dispatch => {
-  await maintenancetrackerapp.get("/api/v1/users/signout");
+  await maintenancetrackerapp.get("/users/signout");
   localStorage.setItem("jwt", "");
   dispatch({
     type: SIGN_OUT,
@@ -34,7 +34,7 @@ export const getMe = () => async dispatch => {
   axios.defaults.headers.common[
     "Authorization"
   ] = `Bearer ${localStorage.getItem("jwt")}`;
-  const res = await maintenancetrackerapp.get("/api/v1/users/Me");
+  const res = await maintenancetrackerapp.get("/users/Me");
   dispatch({
     type: GET_ME,
     payload: res.data.data.user
